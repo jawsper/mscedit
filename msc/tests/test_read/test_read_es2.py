972dc9a26b71bd4ad7169a2223614c35d3bbf467
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2026-01-09 22:26:50 +0100

    test(msc): add more tests, also for unity types

--- msc/tests/test_read/test_read_es2.py
+++ msc/tests/test_read/test_read_es2.py
@@ -1,5 +1,13 @@
 from msc.es2.reader import ES2Reader
 
+from msc.es2.unity import (
+    Color,
+    Mesh,
+    Quaternion,
+    Vector3,
+    Texture2D,
+    Transform,
+)
 
 def test_read_file():
     with open("msc/tests/data/simple.txt", "rb") as f:
@@ -11,3 +19,21 @@ def test_read_file():
         assert data["string"].value == "hello world"
         assert data["int32"].value == 1
         assert data["float"].value == 1.0
+
+def test_read_complex_file():
+    with open("msc/tests/data/complex.txt", "rb") as f:
+        reader = ES2Reader(f)
+        data = reader.read_all()
+
+        assert data["bool"].value is True
+        assert data["byte"].value == 1
+        assert data["string"].value == "hello world"
+        assert data["int32"].value == 1
+        assert data["float"].value == 1.0
+
+        assert isinstance(data["color"].value, Color)
+        assert isinstance(data["mesh"].value, Mesh)
+        assert isinstance(data["quaternion"].value, Quaternion)
+        assert isinstance(data["vector3"].value, Vector3)
+        assert isinstance(data["texture2d"].value, Texture2D)
+        assert isinstance(data["transform"].value, Transform)
--- msc/tests/test_write/test_write_es2.py
+++ msc/tests/test_write/test_write_es2.py
@@ -1,17 +1,34 @@
 from io import BytesIO
 from msc.es2.enums import ES2ValueType
 from msc.es2.types import ES2Field
+from msc.es2.unity import (
+    Color,
+    Mesh,
+    MeshSettings,
+    Quaternion,
+    Vector3,
+    Texture2D,
+    Transform,
+)
 from msc.es2.writer import ES2Writer
 
 
 def test_write_file():
     with BytesIO() as f:
         writer = ES2Writer(f)
-        writer.save("bool", ES2Field.from_value_type(ES2ValueType.bool, True))
         writer.save("byte", ES2Field.from_value_type(ES2ValueType.byte, 1))
+        writer.save("bool", ES2Field.from_value_type(ES2ValueType.bool, True))
         writer.save("string", ES2Field.from_value_type(ES2ValueType.string, "hello world"))
         writer.save("int32", ES2Field.from_value_type(ES2ValueType.int32, 1))
         writer.save("float", ES2Field.from_value_type(ES2ValueType.float, 1.0))
+
+        # writer.save("boneweight", ES2Field.from_value_type(ES2ValueType.boneweight, Color(0,0,0,0)))
+        writer.save("color", ES2Field.from_value_type(ES2ValueType.color, Color(0,0,0,0)))
+        writer.save("mesh", ES2Field.from_value_type(ES2ValueType.mesh, Mesh(settings=MeshSettings(b''))))
+        writer.save("quaternion", ES2Field.from_value_type(ES2ValueType.quaternion, Quaternion(0,0,0,0)))
+        writer.save("vector3", ES2Field.from_value_type(ES2ValueType.vector3, Vector3(0,0,0)))
+        writer.save("texture2d", ES2Field.from_value_type(ES2ValueType.texture2d, Texture2D(b'')))
+        writer.save("transform", ES2Field.from_value_type(ES2ValueType.transform, Transform()))
         writer.save_all()
 
-        assert len(f.getvalue()) > 0
+        assert len(f.getvalue()) == 338
