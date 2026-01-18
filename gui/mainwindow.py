d908dfef07e2801aaba97d2521c37875bbcfb664
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2026-01-18 15:19:48 +0100

    feat(gui): allow multiple open files at the same time
    This moves the QTreeView to TableWidget, and updates the MainWindow to handle opening and closing of files.

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,3 +1,4 @@
+from functools import partial
 import logging
 import os
 import shutil
@@ -5,18 +6,23 @@ import sys
 import tempfile
 from typing import cast
 
-from PyQt6.QtCore import Qt, QModelIndex, QSortFilterProxyModel
-from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QDialog
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QMainWindow,
+    QApplication,
+    QFileDialog,
+    QTabWidget,
+)
 from PyQt6.uic.load_ui import loadUi
 
 from msc.es2 import ES2Reader, ES2Writer
 from msc.es2.reader import ES2Field
-from gui.models import TreeModel
 
-from .dialogs import BoltCheckerDialog, EditDialog, ErrorDialog, MapViewDialog
-
+from .dialogs import BoltCheckerDialog, ErrorDialog, MapViewDialog
+from .widgets.table import TableWidget
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def _copy_file(src, dst):
@@ -32,25 +38,25 @@ def _copy_file(src, dst):
 
 
 class MainWindow(QMainWindow):
-    file_data: dict[str, ES2Field] | None
+    open_file_dir: str
+    open_files: set[str]
 
     def __init__(self):
         super().__init__()
 
-        self.filename = None
-        self.data_changed = False
-        self.file_data = None
-
         self.open_file_dir = "."  # os.path.expanduser("~")
         match sys.platform:
             case "win32":
-                self.open_file_dir = os.path.expandvars("%APPDATA%/../LocalLow/Amistech/My Winter Car")
+                self.open_file_dir = os.path.expandvars(
+                    "%APPDATA%/../LocalLow/Amistech/My Winter Car"
+                )
             case "darwin":
                 pass
             case "linux":
                 pass
             case _:
                 pass
+        self.open_files = set()
 
         self.ui = loadUi("gui/MainWindow.ui", self)
         assert self.ui
@@ -64,127 +70,192 @@ class MainWindow(QMainWindow):
         self.ui.action_ShowMap.triggered.connect(self.show_map)
         self.ui.action_BoltChecker.triggered.connect(self.show_boltchecker)
 
-        self.datamodel = QSortFilterProxyModel()
-        self.datamodel.setFilterCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
-        self.datamodel.setSortCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
-        self.ui.treeView.setSortingEnabled(False)
-        self.ui.treeView.setModel(self.datamodel)
-        self.ui.treeView.setSortingEnabled(True)
+        tab_widget = cast(QTabWidget, self.ui.tabWidget)
+        tab_widget.setTabsClosable(True)
+        tab_widget.currentChanged.connect(self.current_tab_changed)
+        tab_widget.tabCloseRequested.connect(self.tab_close_requested)
 
-        self.ui.treeView.doubleClicked.connect(self.treeView_doubleClicked)
-        self.ui.treeView.selectionModel().selectionChanged.connect(
-            self.treeView_selectionChanged
-        )
         self.ui.searchField.textChanged.connect(self.searchField_textChanged)
 
     def menu_open(self):
+        """
+        Slot that gets triggered by the "Open" menu item.
+        """
         fname = QFileDialog.getOpenFileName(self, "Open file", self.open_file_dir)
         if fname[0]:
             self.open_file(fname[0])
 
     def open_file(self, filename: str):
-        self.filename = filename
-        self.open_file_dir = os.path.dirname(self.filename)
+        if filename in self.open_files:
+            return
+
+        self.open_file_dir = os.path.dirname(filename)
         try:
             with open(filename, "rb") as f:
                 reader = ES2Reader(f)
-                self.file_data = reader.read_all()
+                file_data = reader.read_all()
         except Exception as e:
             logger.exception("Failed to load file")
             return self.show_error(e)
-        self.update_tree(self.file_data)
-        self.set_changed(False)
-        self.ui.action_Close.setEnabled(True)
 
-    def set_changed(self, changed: bool = True):
-        self.data_changed = changed
-        self.ui.action_Save.setEnabled(self.data_changed)
+        self.open_files.add(filename)
+
+        self.open_new_tab(filename, file_data)
+
+    def open_new_tab(self, filename: str, file_data: dict[str, ES2Field]):
+        tab_widget = cast(QTabWidget, self.ui.tabWidget)
+        table_widget = TableWidget(filename=filename, data=file_data)
+        index = tab_widget.addTab(table_widget, os.path.basename(filename))
+        table_widget.data_changed.connect(
+            partial(self.set_data_changed, filename=filename, tab_index=index)
+        )
+        table_widget.datamodel.setFilterWildcard(self.ui.searchField.text())
+        table_widget.datamodel.setFilterCaseSensitivity(
+            Qt.CaseSensitivity.CaseSensitive
+            if self.ui.action_CaseSensitive.isChecked()
+            else Qt.CaseSensitivity.CaseInsensitive
+        )
+        tab_widget.setCurrentIndex(index)
+
+    def current_tab_changed(self, index: int):
+        """
+        Slot that gets triggered when the tabWidget changed tabs.
+        """
+        tab = cast(TableWidget | None, self.ui.tabWidget.widget(index))
+        if tab:
+            self.ui.action_Save.setEnabled(tab.changed)
+            self.ui.action_Close.setEnabled(True)
+        else:
+            self.ui.action_Save.setEnabled(False)
+            self.ui.action_Close.setEnabled(False)
+
+    def tab_close_requested(self, index: int):
+        """
+        Slot that gets triggered by the tab close button pressed on a tab in the tabWidget
+        """
+        self._close_tab_by_index(index)
+
+    def set_data_changed(
+        self, changed: bool = True, *, filename: str = "", tab_index: int = -1
+    ):
+        """
+        Slot that gets triggered when the data_changed signal on a TableWidget gets triggered.
+        """
+        logger.info("CHANGED %s %d %d", filename, tab_index, changed)
+        tab_widget = cast(QTabWidget, self.ui.tabWidget)
+        tab_widget.setTabText(
+            tab_index, f"{os.path.basename(filename)}{' *' if changed else ''}"
+        )
+
+        self.ui.action_Save.setEnabled(changed)
 
     def menu_save(self):
-        assert self.filename
-        assert self.file_data
+        """
+        Slot that gets triggered by the "Save" menu item.
+        """
+        index = self._current_tab_index()
+        tab = self._current_tab()
+        if tab is None:
+            return
+        assert tab.filename
+        assert tab.file_data
         for i in range(100):
-            backup_filename = f"{self.filename}.{i}"
+            backup_filename = f"{tab.filename}.{i}"
             if not os.path.exists(backup_filename):
-                _copy_file(self.filename, backup_filename)
+                _copy_file(tab.filename, backup_filename)
                 break
         else:
             raise Exception("Too many backups!")
 
         with tempfile.NamedTemporaryFile("wb", delete_on_close=False) as f:
             writer = ES2Writer(f.file)
-            for k, v in self.file_data.items():
+            for k, v in tab.file_data.items():
                 writer.save(k, v)
             writer.save_all()
 
             f.close()
 
-            _copy_file(f.name, self.filename)
+            _copy_file(f.name, tab.filename)
 
-        self.set_changed(False)
+        self.set_data_changed(False, filename=tab.filename, tab_index=index)
 
     def menu_close(self):
-        assert self.ui
-        self.ui.action_Save.setEnabled(False)
-        self.ui.action_Close.setEnabled(False)
-        self.filename = None
-        self.file_data = None
-        self.update_tree(None)
+        """
+        Slot that gets triggered by the "Close" menu item.
+        """
+        index = cast(QTabWidget, self.ui.tabWidget).currentIndex()
+        self._close_tab_by_index(index)
+
+    def _close_tab_by_index(self, index: int):
+        tab_widget = cast(QTabWidget, self.ui.tabWidget)
+        tab = cast(TableWidget | None, tab_widget.widget(index))
+        if tab:
+            self.open_files.remove(tab.filename)
+        tab_widget.removeTab(index)
 
     def menu_search_mode(self, action: bool):
-        self.datamodel.setFilterCaseSensitivity(
-            Qt.CaseSensitivity.CaseSensitive
-            if action
-            else Qt.CaseSensitivity.CaseInsensitive
-        )
-
-    def update_tree(self, file_data: dict[str, ES2Field] | None = None):
-        assert self.ui
-        self.datamodel.setSourceModel(TreeModel(file_data))
-        self.ui.treeView.sortByColumn(0, Qt.SortOrder.AscendingOrder)
-        self.ui.treeView.resizeColumnToContents(0)
-
-    def treeView_selectionChanged(self):
-        pass
-        # indexes = self.ui.treeView.selectionModel().selection().indexes()
-        # if len(indexes) > 0:
-        #     index = indexes[0]
-        #     tag = self.datamodel.data(index)
-        #     print('selectionChanged')
-        #     print(self.file_data[item].header.value_type)
-        #     print(self.file_data[item].value)
-
-    def treeView_doubleClicked(self):
-        assert self.ui
-        index: QModelIndex = self.ui.treeView.selectionModel().selection().indexes()[0]
-        self.edit_index(index)
-
-    def edit_index(self, index: QModelIndex):
-        assert self.ui
-        assert self.file_data
-        tag = cast(str, index.data())
-        dialog = EditDialog(tag, self.file_data[tag], self)
-        result = dialog.exec()
-        if result == QDialog.DialogCode.Accepted:
-            dialog_result = dialog.get_value()
-            if self.file_data[tag].value == dialog_result:
-                return
-            self.set_changed()
-            self.file_data[tag].value = dialog_result
-            value_index = self.datamodel.index(index.row(), 2)
-            self.datamodel.setData(value_index, dialog_result)
+        """
+        Slot that gets triggered by the action_CaseSensitivity action in the menuSearch_mode menu item.
+        """
+        for tab in self._all_tabs():
+            tab.datamodel.setFilterCaseSensitivity(
+                Qt.CaseSensitivity.CaseSensitive
+                if action
+                else Qt.CaseSensitivity.CaseInsensitive
+            )
 
     def searchField_textChanged(self, text: str):
-        self.datamodel.setFilterWildcard(text)
+        """
+        Slot that gets triggered by the textChanged signal of searchField.
+        """
+        for tab in self._all_tabs():
+            tab.datamodel.setFilterWildcard(text)
 
     def show_map(self):
-        dialog = MapViewDialog(self.file_data, self)
-        dialog.exec()
+        """
+        Slot that gets triggered by the "Show map" menu item.
+        """
+        tab = self._current_tab()
+        if tab:
+            dialog = MapViewDialog(tab.file_data, self)
+            dialog.exec()
 
     def show_boltchecker(self):
-        dialog = BoltCheckerDialog(self.file_data, self)
-        dialog.exec()
+        """
+        Slot that gets triggered by the "Boltchecker" menu item.
+        """
+        tab = self._current_tab()
+        if tab:
+            dialog = BoltCheckerDialog(tab.file_data, self)
+            dialog.exec()
 
     def show_error(self, exception: Exception):
+        """
+        Helper function to display exceptions.
+        """
         dialog = ErrorDialog(exception, self)
         dialog.exec()
+
+    def _current_tab_index(self) -> int:
+        """
+        Helper to get the index of the currently selected tab, or -1 if no tabs.
+        """
+        tab_widget = cast(QTabWidget, self.ui.tabWidget)
+        return tab_widget.currentIndex()
+
+    def _current_tab(self) -> TableWidget | None:
+        """
+        Helper to get the TableWidget of the currently selected tab, or None if no tabs.
+        """
+        tab_widget = cast(QTabWidget, self.ui.tabWidget)
+        return cast(TableWidget | None, tab_widget.currentWidget())
+
+    def _all_tabs(self) -> list[TableWidget]:
+        """
+        Helper function to get all TableWidgets of all tabs.
+        """
+        tab_widget = cast(QTabWidget, self.ui.tabWidget)
+        tabs = []
+        for index in range(tab_widget.count()):
+            tabs.append(tab_widget.widget(index))
+        return tabs
---
+++ gui/widgets/table.py
@@ -0,0 +1,58 @@
+from typing import cast
+
+from PyQt6.QtCore import Qt, QModelIndex, QSortFilterProxyModel, pyqtSignal
+from PyQt6.QtWidgets import QDialog, QWidget, QTreeView, QVBoxLayout
+
+from msc.es2.types import ES2Field
+
+from ..dialogs import EditDialog
+from ..models import TreeModel
+
+
+class TableWidget(QWidget):
+    filename: str
+    file_data: dict[str, ES2Field]
+    changed: bool = False
+
+    data_changed = pyqtSignal(bool)
+
+    def __init__(self, parent=None, *, filename: str, data: dict[str, ES2Field]):
+        super().__init__(parent)
+
+        self.filename = filename
+        self.file_data = data
+        self.changed = False
+
+        layout = QVBoxLayout()
+        self.tree_view = QTreeView()
+        layout.addWidget(self.tree_view)
+        self.setLayout(layout)
+
+        self.datamodel = QSortFilterProxyModel()
+        self.datamodel.setFilterCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        self.datamodel.setSortCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+
+        self.tree_view.setModel(self.datamodel)
+
+        self.datamodel.setSourceModel(TreeModel(self.file_data))
+        self.tree_view.sortByColumn(0, Qt.SortOrder.AscendingOrder)
+        self.tree_view.resizeColumnToContents(0)
+
+        self.tree_view.doubleClicked.connect(self.treeView_doubleClicked)
+
+    def treeView_doubleClicked(self, index: QModelIndex):
+        self.edit_index(index.siblingAtColumn(0))
+
+    def edit_index(self, index: QModelIndex):
+        tag = cast(str, index.data())
+        dialog = EditDialog(tag, self.file_data[tag], self)
+        result = dialog.exec()
+        if result == QDialog.DialogCode.Accepted:
+            dialog_result = dialog.get_value()
+            if self.file_data[tag].value == dialog_result:
+                return
+            self.changed = True
+            self.data_changed.emit(True)
+            self.file_data[tag].value = dialog_result
+            value_index = index.siblingAtColumn(2)
+            self.datamodel.setData(value_index, dialog_result)
